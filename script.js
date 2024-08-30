9abad945337a93b1d69190b95cfe669f76c1090e
Dharmendra Singh <singhdhas9559604443a@gmail.com>
2024-08-30 10:45:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,112 @@
+// alert("Welcome to Hell");
+// var a = 1;
+// console.log(a);
+// document.write("U use for javascript");
+// wap in js to take userinput and print it
+// var a;
+// a = prompt("Enter Your Name");
+
+// alert(
+//     "Your name is =  " + a);
+// add two no
+// var a, b, c, si;
+// a = parseInt(prompt("Enter First No."));
+// b = parseInt(prompt("Enter second No."));
+// c = parseInt(prompt("Enter third No."));
+// si = (a * b * c) / 100;
+// alert("Simple Interest is = " + (si));
+// var a, b, c, si;
+// a = parseInt(prompt("Enter First No."));
+// alert("Simple Interest is = " + Math.pow(a, 2));
+//if
+// var a, b, c, si;
+// a = parseInt(prompt("Enter Age."));
+
+// if (a > 18) {
+
+//     alert("You are aligible for Vote");
+// } 
+//even odd
+
+// var a, b, c, si;
+// a = parseInt(prompt("Enter Any No. "));
+
+// if (a % 2 == 0) {
+
+//     alert("Even No");
+// } else {
+//     alert("Odd No");
+
+// }
+// ifwe press one add we press sub if we press 3 multiply we press 4 divide orenter novar a, b, c, si;
+// one = parseInt(prompt("Enter first No. "));
+// two = parseInt(prompt("Enter Second No. "));
+// a = prompt("Enter choise No. in 1 to add, 2 to sub, 3 to mul, 4 to div");
+
+
+// if (a == '1') {
+
+//     alert("sum = " + (one + two));
+// } else if (a == '2') {
+//     alert("Sub = " + (one - two));
+// } else if (a == '3') {
+//     alert("Mul = " + (one * two));
+// } else if (a == '4') {
+//     alert("Div = " + (one / two));
+
+// } else {
+//     alert("Invalid");
+// }
+// one = parseInt(prompt("Enter first No. "));
+// two = parseInt(prompt("Enter Second No. "));
+
+
+
+// if (one > two) {
+
+//     alert("ur in profit ");
+// } else if (one < two) {
+//     alert("ur in loss ");
+// } else {
+//     alert("Same");
+// }
+// use of switch
+// var n, name;
+
+// a = prompt("Enter Ur Name ");
+// n = Math.floor(Math.random() * 12);
+// switch (n) {
+//     case 0:
+//         {
+//             alert("Heloo" + a + "ur eng");
+//             break;
+
+
+//         }
+//     case 1:
+//         {
+//             alert("Heloo" + a + "ur dr.");
+//             break;
+
+//         }
+//     default:
+//         {
+//             alert("pls enter valid no. ");
+//             break;
+//         }
+// }
+//1 to 100 no print in javascript
+
+// b = parseInt(prompt("Enter number"));
+// for (a = 1; a <= 10; a++) {
+//     document.write("\t<br> " + a + "*" + b + "=" + (a * b) + "<hr>");
+// }
+// b = parseInt(prompt("Enter number"));
+// for (a = 1; a <= 10; a++) {
+//     if () {}
+
+// }
+//confirm fun
+msg = confirm("Are you sure?");
+
+alert("getout from class");
